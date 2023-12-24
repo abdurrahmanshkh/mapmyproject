5063e93a0a940aa9db329c8d0dff4b0073bce0a6
Suyash <suyash.saraf@somaiya.edu>
2023-12-24 18:50:02 +0530

    Added and tested user registration, login and logout

---
+++ api/controllers/authenticaton.js
@@ -0,0 +1,75 @@
+import {db} from "../connect.js"
+import bcrpyt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+export const register = (req, res) => {
+    const { username, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json("Invalid email format");
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json("Password must be at least 8 characters long");
+    }
+
+    const query = "SELECT * FROM users WHERE name=?";
+    db.query(query, [username], (err, data) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        if (data.length) {
+            return res.status(409).json("User already exists");
+        }
+
+        const hashedPassword = bcrpyt.hashSync(password, bcrpyt.genSaltSync(10));
+
+        // Add the current date to the created column
+        const currentDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+        const insertQuery = "INSERT INTO users(name, email, hashedPassword, created) VALUES (?, ?, ?, ?)";
+        const values = [username, email, hashedPassword, currentDate];
+
+        db.query(insertQuery, values, (err, data) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            return res.status(200).json("User has been created");
+        });
+    });
+};
+
+export const login=(req,res)=>{
+    const query="SELECT * FROM users WHERE name=?"
+    db.query(query,[req.body.username],(err,data)=>{
+        if(err) {
+            return res.status(500).json(err)
+        }
+        if (data.length===0){
+            return res.status(404).json("User not found")
+        }
+        const hashedPassword=bcrpyt.compareSync(req.body.password,data[0].hashedPassword)
+        if(!hashedPassword){
+            return res.status(400).json("incorrect password")
+        }
+
+        const token=jwt.sign({id:data[0].id},"secretkey")
+        const{password,...others}=data[0]
+
+        res
+        .cookie("accessToken",token,{
+            httpOnly: true,
+        })
+        .status(200)
+        .json(others);
+    })    
+}
+
+export const logout=(req,res)=>{
+    res.clearCookie("accessToken",{
+        secure:true,
+        sameSite:"none"
+    }).status(200).json("User logged out successfully")
+}
--- api/index.js
+++ api/index.js
@@ -1,1 +1,20 @@
-import {express} from express
+import express from "express"
+import cors from "cors"
+import authRoute from "./routes/authRoute.js"
+import cookieParser from "cookie-parser"
+const app=express()
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials",true)
+    next()
+})
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRoute)
+app.listen(8080,()=>{
+    console.log("API working")
+})
