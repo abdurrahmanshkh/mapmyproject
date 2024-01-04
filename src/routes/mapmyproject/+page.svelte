05ba115ee7dabfd07c7787f20156e03721cd29c1
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-01-04 13:24:52 +0530

    Created Home Page as mapmyproject page

---
+++ src/routes/mapmyproject/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    // Import Flowbite styles
+
+    // Component logic goes here
+</script>
+
+<div class="container">
+    <h1>Welcome to the Project Management Website</h1>
+    <!-- Add your content here -->
+</div>
+
+<style>
+    /* Add your custom styles here */
+</style>
