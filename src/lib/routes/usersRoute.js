3653e990cc2caedae8d540fce8ee6df074f62a42
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-01-13 14:29:30 +0530

    Revert "Added Backend Files"
    This reverts commit 90ffc9582f2129bdd30c0fabbf4506b15dbe14aa.

--- src/lib/routes/usersRoute.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-const router=express.Router()
-import { requireAuth } from "../helpers.js"
-import { getProfile } from "../controllers/users.js"
-router.get("/profile",requireAuth,getProfile)
-export default router
