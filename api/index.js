de95ddc78bc603da6270b419f3179b82ad02b832
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-01-16 12:35:41 +0530

    Added Backend

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+import authRoute from "./routes/authRoute.js"
+import projectRoute from "./routes/projectRoute.js"
+import taskRoute from "./routes/taskRoute.js"
+import userRoute from "./routes/usersRoute.js"
+const app=express()
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials",true)
+    next()
+})
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoute)
+app.use("/api/project",projectRoute)
+app.use("/api/project",taskRoute)
+app.use("/api",userRoute)
+//localhost:8080/api/project/create uses requireAuth middleware which only allows authenticated users to create new projects 
+app.use("/api/project",projectRoute)
+app.listen(8080,()=>{
+    console.log("API working")
+})
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,37 @@
+<!-- App.svelte -->
+<script>
+    let username = '';
+    let password = '';
+    let email='';
+
+    const registerUser = async () => {
+        const response = await fetch('http://localhost:8080/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                email
+            }),
+        });
+
+        const data = await response.json();
+        console.log(data);
+    };
+</script>
+
+<form on:submit={registerUser}>
+    <label for="username">Username:</label>
+    <input type="text" bind:value={username} />
+
+    <label for="password">Password:</label>
+    <input type="password" bind:value={password} />
+
+    <label for="email">Email:</label>
+    <input type="email" bind:value={email} />
+
+
+    <button type="submit">Register</button>
+</form>
