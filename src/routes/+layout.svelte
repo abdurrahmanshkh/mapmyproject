37b7a8e79f3c04179a9a9a8799d9607bb3b6e969
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-04-01 00:52:47 +0530

    Separated Header and Footer Files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,21 @@
 <script>
 	import '../app.pcss';
-  	import { Footer, FooterCopyright, Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode} from 'flowbite-svelte';
+	import Header from './header.svelte';
+	import Footer from './footer.svelte';
 </script>
 
 <svelte:head>
-		<title>MapMyProject</title>
+	<title>MapMyProject</title>
 </svelte:head>
 
 <div class="app">
 
-	<header class="flex-none mx-auto w-full bg-white dark:bg-gray-900 left-0 top-0 z-30 border-b-2 border-gray-300 dark:border-gray-700">
-		<div class="w-full px-0 py-3 mx-auto lg:flex lg:justify-between max-w-8xl lg:px-3">
-			<Navbar>
-				<NavBrand href="/">
-					<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">MapMyProject</span>
-				</NavBrand>
-				<NavHamburger />
-				<NavUl>
-					<NavLi href="/" active={true}>Home</NavLi>
-					<NavLi href="/projects">Projects</NavLi>
-					<NavLi href="/dashboard">Dashboard</NavLi>					
-				</NavUl>
-				<DarkMode/>
-			</Navbar>
-		</div>
-	</header>
-	
+	<Header />
+
 	<main class="bg-white dark:bg-gray-900" style="min-height: calc(100vh - 145px);">
-		<slot/>
+		<slot />
 	</main>
 
-
-	<footer class="flex-none mx-auto w-full bg-white dark:bg-gray-900 border-t-2 border-gray-300 dark:border-gray-700">
-		<div class="w-full px-0 py-2 mx-auto lg:flex lg:justify-between max-w-8xl lg:px-3">
-			<Navbar>
-				<Footer rounded color="form" class="w-full flex items-center text-center justify-center py-3">
-					<FooterCopyright href="/" by="MapMyProject™" year={2024} />
-				</Footer>
-			</Navbar>
-		</div>
-	</footer>	
+	<Footer />
 
 </div>
---
+++ src/routes/footer.svelte
@@ -0,0 +1,13 @@
+<script>
+      	import { Footer, FooterCopyright, Navbar } from 'flowbite-svelte';
+</script>
+
+<footer class="flex-none mx-auto w-full bg-white dark:bg-gray-900 border-t-2 border-gray-300 dark:border-gray-700">
+    <div class="w-full px-0 py-2 mx-auto lg:flex lg:justify-between max-w-8xl lg:px-3">
+        <Navbar>
+            <Footer rounded color="form" class="w-full flex items-center text-center justify-center py-3">
+                <FooterCopyright href="/" by="MapMyProject™" year={2024} />
+            </Footer>
+        </Navbar>
+    </div>
+</footer>
